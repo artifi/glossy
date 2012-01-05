5904e869ba1656727e1cc70193b23430b3c3a259
Maciek <maciek@maciek-VirtualBox.(none)>
2012-01-05 13:31:18 +0100

    Performance optimalizations. Code is uglier now... but performance increased up to 40% with produce. Small bugfixes. Small changes to tests (date time issue still remaning for BSD format tests).

--- test/produce.js
+++ test/produce.js
@@ -24,16 +24,20 @@ var invalidProducer = new producer({
 });
 assert.notEqual(invalidProducer, 'invalid producer is null');
 
+var dateObject = new Date(1234567890000);
+dateObject.getTimezoneOffset = function(){
+    return 0;
+};
 var msg = syslogProducer.produce({
 	facility: 'local4',
 	severity: 'error',
 	host: 'localhost',
 	app_id: 'sudo',
 	pid: '123',
-	date: new Date(1234567890000),
+	date: dateObject,
 	message: 'Test Message'
 });
-assert.equal(msg, "<163>1 2009-01-13T23:31:30.00Z localhost sudo 123 - - Test Message",'Valid message returned');
+assert.equal(msg, "<163>1 2009-02-13T23:31:30.00Z localhost sudo 123 - Test Message",'Valid message returned');
 
 syslogProducer.produce({
 	facility: 'audit',
@@ -41,10 +45,10 @@ syslogProducer.produce({
 	host: '127.0.0.1',
 	app_id: 'sudo',
 	pid: '419',
-	date: new Date(1234567890000),
+	date: dateObject,
 	message: 'Test Message'
 }, function(cbMsg) {
-	assert.equal(cbMsg, '<107>1 2009-01-13T23:31:30.00Z 127.0.0.1 sudo 419 - - Test Message', 'Valid message in callback returned');
+	assert.equal(cbMsg, '<107>1 2009-02-13T23:31:30.00Z 127.0.0.1 sudo 419 - Test Message', 'Valid message in callback returned');
 });
 
 BSDProducer.produce({
@@ -53,7 +57,7 @@ BSDProducer.produce({
 	host: '127.0.0.1',
 	app_id: 'sudo',
 	pid: '419',
-	date: new Date(1234567890000),
+	date: dateObject,
 	message: 'Test Message'
 }, function(cbMsg){
 	assert.equal(cbMsg, '<107>Feb 13 23:31:30 127.0.0.1 sudo[419]: Test Message');
